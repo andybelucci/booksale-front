537ec824abaf1c67d07374d460fdc7a44f7aed8f
andybelucci <beluccianderson@gmail.com>
2023-08-29 12:12:52 -0300

    UPDATE para Livro implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/views/home/home.component';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
 import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
+import { LivroCreateComponent } from './components/views/livro/livro-create/livro-create.component';
+import { LivroUpdateComponent } from './components/views/livro/livro-update/livro-update.component';
 
 const routes: Routes = [
   {
@@ -32,6 +34,14 @@ const routes: Routes = [
   {
     path: 'categorias/:id_cat/livros',
     component: LivroReadAllComponent
+  },
+  {
+    path: 'categorias/:id_cat/livros/create',
+    component: LivroCreateComponent
+  },
+  {
+    path: 'categorias/:id_cat/livros/:id/update',
+    component: LivroUpdateComponent
   }
 ];
 
